17756375be0edf299eb2903fca233420dd8e9cc9
Long Nguyen <rongdat1710@gmail.com>
2022-01-18 16:09:59 -0500

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require jquery_ujs
-//= require jquery.turbolinks
 //= require activestorage
 //= require_tree 
 //= require jquery.mixitup.min
 //= require bootstrap.min
+//= require jquery.turbolinks
