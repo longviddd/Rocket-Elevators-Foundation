5aa3f03d2dbb642feebbadd7f5557518f0f437a4
Long Nguyen <rongdat1710@gmail.com>
2022-01-20 19:55:04 -0500

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require activestorage
-//= require_tree 
 //= require jquery.mixitup.min
 //= require bootstrap.min
 //= require jquery.turbolinks
+//= require turbolinks
+//= require_tree 
+
